6cb118c7a6068b6da7ff86a6d274cc11a05de137
Senny Chu <senny.share@gmail.com>
2014-01-05 10:59:15 +0800

    new file:   Library/Formula/auto-apt.rb 	new file:   Library/Formula/autoconf.rb_bk 	new file:   Library/Formula/grub.rb

---
+++ Library/Formula/auto-apt.rb
@@ -0,0 +1,36 @@
+require 'formula'
+
+# this formula is create by senny!!!
+
+class AutoApt < Formula
+  homepage ''
+  url 'http://ftp.de.debian.org/debian/pool/main/a/auto-apt/auto-apt_0.3.22.tar.gz'
+  sha1 '00a8ce34cdcb899a780f5c33ccca1b79ef0bb81b'
+
+  # depends_on 'cmake' => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test auto-apt`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "--version"`.
+    system "false"
+  end
+end
